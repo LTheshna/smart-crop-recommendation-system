ef0cbe4dd435cbde136d23cf5dacbd6e77a52aff
Theshna <l.theshna8@.com>
2026-05-14 17:10:05 +0530

    connected frontend to deployed backend

--- crop-ui/src/services/api.js
+++ crop-ui/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://smart-crop-backend-l8p2.onrender.com",
 });
 
 export const predictCrop = async (formData) => {
